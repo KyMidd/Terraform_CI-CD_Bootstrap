fa1a8723745f0998fd906a7bfaf216f92ae879a0
Kyler Middleton <kymidd@gmail.com>
2019-11-17 15:57:29 -0700

    Initial commit

---
+++ main.tf
@@ -0,0 +1,28 @@
+# Require TF version to be same as or greater than 0.12.13
+terraform {
+  required_version = ">=0.12.13"
+  #backend "s3" {
+  #  bucket         = "your_globally_unique_bucket_name"
+  #  key            = "terraform.tfstate"
+  #  region         = "us-east-1"
+  #  dynamodb_table = "aws-locks"
+  #  encrypt        = true
+  #}
+}
+
+# Download any stable version in AWS provider of 2.36.0 or higher in 2.36 train
+provider "aws" {
+  region  = "us-east-1"
+  version = "~> 2.36.0"
+}
+
+# Call the seed_module to build our ADO seed info
+module "bootstrap" {
+  source                      = "./modules/bootstrap"
+  name_of_s3_bucket           = "your_globally_unique_bucket_name"
+  dynamo_db_table_name        = "aws-locks"
+  iam_user_name               = "IamUser"
+  ado_iam_role_name           = "IamRole"
+  aws_iam_policy_permits_name = "IamPolicyPermits"
+  aws_iam_policy_assume_name  = "IamPolicyAssume"
+}
---
+++ modules/bootstrap/bootstrap_variables.tf
@@ -0,0 +1,10 @@
+##
+# Define variables for Azure DevOps Seed Module
+##
+
+variable "name_of_s3_bucket" {}
+variable "dynamo_db_table_name" {}
+variable "iam_user_name" {}
+variable "ado_iam_role_name" {}
+variable "aws_iam_policy_permits_name" {}
+variable "aws_iam_policy_assume_name" {}
