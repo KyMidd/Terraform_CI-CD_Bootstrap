76d9b87c2c4dc970b6397dd298d2521fd46d1f43
kymidd@gmail.com <kymidd@gmail.com>
2020-04-28 16:22:16 -0600

    Removing unused variables

--- modules/bootstrap/bootstrap_variables.tf
+++ modules/bootstrap/bootstrap_variables.tf
@@ -4,7 +4,3 @@
 
 variable "name_of_s3_bucket" {}
 variable "dynamo_db_table_name" {}
-variable "iam_user_name" {}
-variable "ado_iam_role_name" {}
-variable "aws_iam_policy_permits_name" {}
-variable "aws_iam_policy_assume_name" {}
